c4302f1c2dade1c5731b006c850ad2f23b6a25a1
KhSudhir2345 <khsudhir2345@gmail.com>
2025-01-11 22:02:18 +0530

    Create utils.py

---
+++ backend/blogs/utils.py
@@ -0,0 +1,16 @@
+from flask_login import current_user
+from backend.models import User
+
+def admin_required(f):
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_admin:
+            return {'error': 'Admin access required'}, 403
+        return f(*args, **kwargs)
+    return decorated_function
+
+def get_online_users():
+    return [
+        {"id": user.id, "name": user.name}
+        for user in User.query.filter_by(is_online=True).all()
+        if user.id != current_user.id
+    ]
