7089c90bd113871d6304a4fe4c64a30d435ac8b5
KhSudhir2345 <khsudhir2345@gmail.com>
2025-01-12 08:54:10 +0530

    Create ProblemDescriptionForm.js

---
+++ frontend/src/ProblemDescriptionForm.js
@@ -0,0 +1,73 @@
+import React,{useState} from "react";
+import "./ProblemDescriptionFile.css";
+import NavAfterLogin from "./NavAfterLogin";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+const ProblemDescriptionForm=() => {
+    const [currentStep,setCurrentStep] = useState(0);
+    const [formData,setFormData] = useState({
+       smile_last_time:"",
+       smile_reason: "", 
+    });
+    const formFields = [
+        {id:"smile_last_time", label:"When did you smile last time(press enter after you type this)",type:"text",required:"true"},
+        {id:"smile_reason", label:"Why Did you lose your smile",type:"text",required:"true"}
+    ]
+    const navigate=useNavigate();
+    const handleChange=(e) => {
+        const { id,value } =e.target;
+        setFormData((prevData) => ({ ...prevData,[id]:value}));
+    };
+    const handleKeyDown = (e) => {
+        if(e.key==="Enter"){
+            e.preventDefault();
+        
+        const currentField=formFields[currentStep];
+        if(currentField.required && !formData[currentField.id]){
+            alert(`${currentField.label} is required`);
+            return;
+        }
+
+        setTimeout(() => {
+            if(currentStep < formFields.length-1){
+                setCurrentStep((prevStep) => prevStep + 1);
+                console.log(currentStep);
+            }
+        },10);
+    }
+    
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const response= await axios.post("http://localhost:8000/auth/problem-page",formData,{headers:{'Content-Type' : 'application/json'},withCredentials:true,});
+            const message=response.data.message;
+            console.log("Form Submitted: ",formData);
+            console.log(message);
+            navigate('/home');
+        } catch(err){
+            console.error("Error posting problem: ",err);
+        }
+    };
+    return(
+        <div className="formContainer">
+            <NavAfterLogin />
+            <form onSubmit={handleSubmit}>
+                {formFields.map((field,index) => (
+                    <div key={field.id} style={{ display:index <= currentStep ? "block" : "none"}}>
+                        <label htmlFor={field.id}>{field.label}</label>
+                        <input id={field.id} type={field.type} value={formData[field.id]} onChange={handleChange} onKeyDown={handleKeyDown} autoFocus={index === currentStep} />
+                    </div>
+
+                ))}
+                {currentStep === formFields.length-1 && (
+                    <button type="submit" className="Problem-Button">Submit</button>
+                )}
+            </form>
+        </div>
+    );
+
+};
+
+export default ProblemDescriptionForm;
+
