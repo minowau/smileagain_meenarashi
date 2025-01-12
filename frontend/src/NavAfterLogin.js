b3ceb3ee24daf46d32cdae00e12348dd0fbcf271
KhSudhir2345 <khsudhir2345@gmail.com>
2025-01-12 08:50:34 +0530

    Create NavAfterLogin.js

---
+++ frontend/src/NavAfterLogin.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./NavAfterLogin.css"
+import { useAuth } from "./contexts/AuthContext";
+
+const NavAfterLogin=React.memo(() => {
+    // let username=localStorage.getItem("user");
+    // const data = JSON.parse(username);
+    // username=data.username || "User";
+    // console.log(username);
+    return(
+        <div className="navContainer">
+            <nav className="TopNavBar">
+                <div className="Logo">
+                    <span className="Smile">Smile</span>
+                    <span className="Again">Again</span>
+                </div>
+                <div className="Links">
+                    <a href="/home">Home</a>
+                    <a href="/activities">Smile Path</a>
+                    <a href="/blogs">Blogs</a>
+                    <a href="/workshops">Workshops</a>
+                    <a href="/chats">Chats</a>
+                    <a href="/about-us">About us</a>
+                    <a href="/myProfile">User</a>
+                </div>
+            </nav>
+        </div>
+    );
+
+});
+
+export default NavAfterLogin;
