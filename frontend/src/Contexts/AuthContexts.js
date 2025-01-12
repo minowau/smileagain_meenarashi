9a54cd715c7b447fb4cd09c63df9487a12721478
KhSudhir2345 <khsudhir2345@gmail.com>
2025-01-12 09:02:17 +0530

    Create AuthContexts.js

---
+++ frontend/src/Contexts/AuthContexts.js
@@ -0,0 +1,41 @@
+import React, { createContext, useState, useEffect, useContext } from "react";
+
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [username,setuserName]=useState("User");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // useEffect(() => {
+    //     const storedUser = localStorage.getItem("user");
+    //     if (storedUser) {
+    //         const parsedUser = JSON.parse(storedUser);
+    //         setUser(parsedUser);
+    //         setIsLoggedIn(true);
+    //     }
+    // }, []);
+
+    const login = (userDetails) => {
+        setIsLoggedIn(true);
+        setuserName(userDetails || "User");
+        localStorage.setItem("user",userDetails);
+    };
+
+    const logout = () => {
+        setUser(null);
+        setIsLoggedIn(false);
+        localStorage.removeItem("user");
+    };
+
+  
+
+    return (
+        <AuthContext.Provider value={{ user, isLoggedIn, login, logout, username }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
